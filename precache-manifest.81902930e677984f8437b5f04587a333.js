bec13942cef35009f54d2912970a21c02d54e070
Kamran <kamran1992051432@gmail.com>
2022-05-28 21:27:25 +0300

    Updates

---
+++ precache-manifest.81902930e677984f8437b5f04587a333.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c96f417591bde6b499c2",
+    "url": "/React-Calculator/static/css/main.c3ed3dfa.chunk.css"
+  },
+  {
+    "revision": "c96f417591bde6b499c2",
+    "url": "/React-Calculator/static/js/main.c96f4175.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/React-Calculator/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "c31a353cd4e7272b42e6",
+    "url": "/React-Calculator/static/js/2.c31a353c.chunk.js"
+  },
+  {
+    "revision": "29aa408227c86f199918",
+    "url": "/React-Calculator/static/js/runtime~main.29aa4082.js"
+  },
+  {
+    "revision": "1f0b09b9aa9ed70502b3daf7ea331584",
+    "url": "/React-Calculator/index.html"
+  }
+];
