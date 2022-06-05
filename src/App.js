93d9515915c116c015cbc2e5538a6a2530e06e31
Kamran <kamran1992051432@gmail.com>
2022-06-05 20:40:06 +0300

    Fixed bags and changed clear button

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import CalcScreen from './components/CalcScreen';
 import React, { useState } from 'react';
 
 function App() {
-
   const [calc, setCalc] = useState("")
   const [result, setResult] = useState("")
+  const [finalResult, setFinalResult] = useState("")
 
   const ops = ['/', '*', '+', '-', '.']
   const btnValues = [
-    ["C", "+-", "%", "/"],
+    ["Del", "+-", "%", "/"],
     ['7', '8', '9', "*"],
     ['4', '5', '6', "-"],
     ['1', '2', '3', "+"],
@@ -20,15 +20,14 @@ function App() {
   const clean = () => {
     setCalc('')
     setResult('')
+    setFinalResult('')
   }
 
   const cal = (fn) => {
     return new Function('return ' + fn)();
   }
 
-
   const updateCalc = (value) => {
-
     if (
       (ops.includes(value) && calc === '') ||
       (ops.includes(value) && ops.includes(calc.slice(-1)))
@@ -39,28 +38,18 @@ function App() {
     if (!ops.includes(value) || value === '=') {
       setResult(cal(calc + value))
     }
-
-    if (ops.includes(value.slice(-1))) {
-      setResult(cal(value.slice(0, -1)));
-    }
-
-    else {
-
-      setResult(cal(value));
-
-    }
-
   }
 
-  const calculate = (e) => {
+  const calculate = () => {
     setResult(cal(calc))
+    setFinalResult(cal(calc))
   }
 
   return (
     <div className="App">
       <div className="container">
 
-        <CalcScreen result={result} calc={calc} />
+        <CalcScreen result={result} calc={calc} finalResult={finalResult} setFinalResult={setFinalResult} />
 
         <div className={style.wrapper}>
 
@@ -69,7 +58,7 @@ function App() {
               btnValues.flat().map((btn, i) => {
                 return (
                   <button className={style.button}
-                    type="button" key={i} onClick={btn === "C" ? clean : () => updateCalc(btn)}>{btn}</button>)
+                    type="button" key={i} onClick={btn === "Del" ? clean : () => updateCalc(btn)}>{btn}</button>)
               })}
             <button type="button" className={style.buttonEqual} onClick={calculate}>=</button>
           </div>
--- src/components/CalcScreen.jsx
+++ src/components/CalcScreen.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import s from './calcScreen.module.css';
 
 const CalcScreen = (props) => {
-    console.log(props)
+    if (props.finalResult) {
+        return (
+            <div>
+                <div>
+                    <span className={s.screenInput}>{props.finalResult}</span>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div>
             <div>
+
                 {props.result ?
                     <span className={props.calc.length + props.result.length < 6 ? s.screenInput : s.screenInputMin &&
                         props.calc.length + props.result.length >= 12 ? s.screenInputSuperMin : s.screenInputMin
@@ -14,18 +24,9 @@ const CalcScreen = (props) => {
                     <span className={s.screenInput}>{props.result} {props.calc || '0'}</span>
                 }
             </div>
-        </div>
+        </div >
+
     )
 }
-
-
-/*
-
-
-         <span className={props.calc.length + props.result.length < 6 ? s.screenInput : s.screenInputMin &&
-                        props.calc.length + props.result.length >= 12 ? s.screenInputSuperMin : s.screenInputMin
-                    }>({parseInt(props.result).toFixed(3)}) {props.calc || '0'}</span>
-
-*/
 
 export default CalcScreen
